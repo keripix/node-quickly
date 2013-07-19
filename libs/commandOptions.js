c7772e429e7e0abd54821e782fa24ef437867d41
A. Akbar Hidayat <keripix@gmail.com>
2013-07-19 21:00:51 +0700

    add some command line options

--- libs/commandOptions.js
+++ libs/commandOptions.js
@@ -1,7 +1,24 @@
-var opts = require("nomnom")
-    .option("version", {
-        abbr: "v",
-        callback: function(){
-            return "quickly version 0.0.1";
-        }
-    });
+module.exports = {
+  version: {
+    abbr: "v",
+    callback: function(){
+      return "quickly version 0.0.1";
+    }
+  },
+  add: {
+    abbr: "a",
+    help: "Add the given key and path to the quickly records"
+  },
+  remove: {
+    abbr: "rm",
+    help: "Remove the given key fromt the quickly records"
+  },
+  edit: {
+    abbr: "e",
+    help: "Set a new path to the given key"
+  },
+  list: {
+    abbr: "ls",
+    help: "List all the available keys and the responding paths"
+  }
+};
--- libs/keyHandler.js
+++ libs/keyHandler.js
@@ -2,21 +2,21 @@ var Configstore = require("configstore"),
     conf = new Configstore("quickly");
 
 exports.save = function(key, path){
-    conf.set(key, path);
+  conf.set(key, path);
 };
 
 exports.get = function(key){
-    return conf.get(key);
+  return conf.get(key);
 };
 
 exports.delete = function(key){
-    conf.del(key);
+  conf.del(key);
 };
 
 exports.list = function(){
-    return conf.all;
+  return conf.all;
 };
 
 exports.removeAll = function(){
-    conf.all = {};
+  conf.all = {};
 };
