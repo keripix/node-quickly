967fb42b70a6a5d15eac659ba5a0f0df25ac2066
A. Akbar Hidayat <keripix@gmail.com>
2013-07-20 12:31:33 +0700

    friendly echo

---
+++ libs/friendlyEcho.js
@@ -0,0 +1,10 @@
+console.log("Please place this inside your ~/.bashrc file:\n"+
+"q(){\n"+
+"  res=$(q.js \"$@\")\n\n"+
+"  if [ -d \"$res\" ]; then\n"+
+"      cd \"$res\"\n"+
+"  else\n"+
+"      echo -e \"$res\"\n"+
+"  fi\n"+
+"}\n\n"+
+"Lastly, please source the ~/.bashrc file\n\n");
