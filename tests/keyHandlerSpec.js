f149d70b4750fa643c42979b0626dea56d01f4d6
A. Akbar Hidayat <keripix@gmail.com>
2013-07-20 10:30:15 +0700

    Edit

--- tests/keyHandlerSpec.js
+++ tests/keyHandlerSpec.js
@@ -1,10 +1,18 @@
 var keyHandler = require("./../libs/keyHandler"),
     exec = require("child_process").exec,
-    expect = require("expect.js");
+    expect = require("expect.js"),
+    fs = require("fs");
 
 describe("Key Handler", function(){
-  it("Should add the key and the path", function(){
+  beforeEach(function(){
     keyHandler.add("a", process.cwd());
+  });
+
+  afterEach(function(){
+    keyHandler.removeAll();
+  });
+
+  it("Should add the key and the path", function(){
     expect(keyHandler.get("a")).to.equal(process.cwd());
   });
 
@@ -14,7 +22,6 @@ describe("Key Handler", function(){
   });
 
   it("Should list all of the keys", function(){
-    keyHandler.add("a", process.cwd());
     keyHandler.add("b", process.cwd());
     keyHandler.add("c", process.cwd());
 
@@ -27,13 +34,18 @@ describe("Key Handler", function(){
 
   it("Should be able to remove all of the saved path", function(){
     keyHandler.removeAll();
-
     expect(keyHandler.list()).to.be.empty();
   });
 
   it("Should resolve the path that was mapped to a key", function(){
-    keyHandler.add("a", ".");
-    expect(keyHandler.get("a")).to.equal(process.cwd());
+    keyHandler.add("b", ".");
+    expect(keyHandler.get("b")).to.equal(process.cwd());
+  });
+
+  it("Should be able to edit an existing key", function(){
+    keyHandler.edit("a", "..");
+    expect(keyHandler.get("a")).to.equal(fs.realpathSync(".."));
+
     keyHandler.delete("a");
   });
 });
