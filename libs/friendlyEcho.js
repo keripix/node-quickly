001e5d47fa97ebf996d41a9beef5527771591a20
A. Akbar Hidayat <keripix@gmail.com>
2013-07-20 12:38:12 +0700

    quick fix

--- libs/friendlyEcho.js
+++ libs/friendlyEcho.js
@@ -1,8 +1,8 @@
 console.log("Please place this inside your ~/.bashrc file:\n"+
 "q(){\n"+
-"  res=$(q.js \"$@\")\n\n"+
+"  res=$(q.js $@)\n\n"+
 "  if [ -d \"$res\" ]; then\n"+
-"      cd \"$res\"\n"+
+"      cd $res\n"+
 "  else\n"+
 "      echo -e \"$res\"\n"+
 "  fi\n"+
