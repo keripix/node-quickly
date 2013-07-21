6576014cdef8aace641c40be85cb21dd037f523a
A. Akbar Hidayat <keripix@gmail.com>
2013-07-21 23:21:23 +0700

    update post installation steps

--- libs/friendlyEcho.js
+++ libs/friendlyEcho.js
@@ -1,4 +1,5 @@
-console.log("Please place this inside your ~/.bashrc file:\n"+
+var color = require("colors");
+console.log("Please place this inside your ~/.bashrc file:\n".bold.yellow+
 "q(){\n"+
 "  res=$(q.js $@)\n\n"+
 "  if [ -d \"$res\" ]; then\n"+
@@ -7,4 +8,8 @@ console.log("Please place this inside your ~/.bashrc file:\n"+
 "      echo -e \"$res\"\n"+
 "  fi\n"+
 "}\n\n"+
-"Lastly, please source the ~/.bashrc file\n\n");
+"Lastly, please source the ~/.bashrc file\n\n".bold.yellow);
+
+console.log("If you want to enable autocompletion, please run these last two steps:\n\n".bold.yellow+
+"q.js --completion >> ~/q.completion.sh\n"+
+"echo 'source ~/q.completion.sh' >> ~/.bashrc\n");
