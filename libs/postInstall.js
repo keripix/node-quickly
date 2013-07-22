0a27c976a2f65853c5f7c447763463bf7b6de232
A. Akbar Hidayat <keripix@gmail.com>
2013-07-22 10:04:41 +0700

    udpate info. btw, autocomplete not working from q

--- libs/postInstall.js
+++ libs/postInstall.js
@@ -4,7 +4,8 @@ var exec = require("child_process").exec;
 exec("which q", function(err, stdout, stderr){
   var qBin = stdout;
   exec("q --completion >> ~/q.completion.sh", function(err, stdout, stderr){
-    console.log("Please add these two on your ~/.bashrc file\n".bold.yellow);
+    console.log("Firstly, please remove any q related function from your ~/.bashrc file".bold.yellow);
+    console.log("Then, please add these two on your ~/.bashrc file\n".bold.yellow);
 
     console.log("source ~/q.completion.sh".green);
     console.log("source ".green + qBin.green);
